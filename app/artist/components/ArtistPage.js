58b0d299e5b40abba458e205616de4c3bbeca232
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2016-07-17 21:35:49 -0400

    Added mapDispatchToProps to ArtistPage.

--- app/artist/components/ArtistPage.js
+++ app/artist/components/ArtistPage.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { fetchArtist } from '../actions'
+import { connect } from 'react-redux';
 
-export default class ArtistPage extends Component {
+class ArtistPage extends Component {
+  componentDidMount() {
+    this.props.fetchArtist();
+  }
+
   render () {
       return (
         <div>Hello World!</div>
       );
     };
 };
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchArtist }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(ArtistPage)
