72cafc56dca63533fa34c0c841db0d3e51566483
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2016-07-18 16:36:53 -0400

    Testing cards.

--- app/api/backend.js
+++ app/api/backend.js
@@ -1,8 +1,8 @@
 export function getArtist() {
   return new Promise(function(resolve, reject){
     if(true) {
-      resolve([{id: 100, name: "Justin Bieber", img_url: null},
-               {id: 101, name: "Adele", img_url: null}]);
+      resolve([{id: 100, name: "Justin Bieber", img_url: "http://www.fuse.tv/image/544533319a9551c444000080/316/316/justin-bieber.jpg"},
+               {id: 101, name: "Adele", img_url: "https://pbs.twimg.com/profile_images/657199367556866048/EBEIl2ol.jpg"}]);
     } else {
       reject(Error("Broke"));
     }
--- app/artist/components/ArtistList.js
+++ app/artist/components/ArtistList.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
-const ArtistList = (props) => {
-  return(
-    <div>
-      {props.artist.map(({ name, id }) => {
-        return <div key={id}>{name}</div>;
-      })}
-    </div>
-  );
+function handleClick() {
+    console.log("hi");
 }
+
+const ArtistList = (props) => (
+    <div onClick={handleClick} style={{width: 500}}>
+<Card style={{flex: .5, margin: 50}}>
+    <CardMedia>
+      <img src="http://lorempixel.com/600/337/nature/" />
+    </CardMedia>
+    <CardTitle title={"hi"} />
+</Card>
+
+<Card style={{flex: .5, margin: 50}}>
+    <CardMedia>
+      <img src="http://lorempixel.com/600/337/nature/" />
+    </CardMedia>
+    <CardTitle title={"hi"} />
+</Card>
+</div>
+    //<div>
+      //{props.artist.map(({ name, id }) => {
+        //return <div key={id}>{name}</div>;
+      //})}
+    //</div>
+);
 
 export default ArtistList;
