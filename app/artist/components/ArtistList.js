5a1ecbd90ad77cc617f0c2aedbf381260d566c32
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2016-07-20 18:37:51 -0400

    Added dialog.

--- app/artist/components/ArtistList.js
+++ app/artist/components/ArtistList.js
@@ -6,14 +6,37 @@ export default class ArtistList extends Component {
   constructor (props) {
     super(props);
     this.state = {open: false};
+
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  };
+
+  handleOpen() {
+    this.setState({open: true});
+  };
+
+  handleClose() {
+    this.setState({open: false});
   };
 
   render() {
     return (
       <div>
         {this.props.artist.map(({name, id, img_url}) => (
-          <div key={id}><Artist name={name} img={img_url} /></div>
-        ))}
+          <div key={id} onClick={this.handleOpen}>
+            <Artist
+              name={name}
+              img={img_url}
+            />
+          </div>
+          ))}
+        <Dialog
+          title="Tracks"
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          Hello World
+        </Dialog>
       </div>
     );
   };
