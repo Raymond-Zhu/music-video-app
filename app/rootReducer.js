1a930f5ec70119b7551414bde8cf94ac5b99f58f
Raymond Zhu <junhao.raymond.zhu@gmail.com>
2016-09-07 22:20:32 -0400

    Added reducer for track.

--- app/rootReducer.js
+++ app/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import artists from './artist/reducer'
+import artists from './artist/reducer';
+import tracks from './track/reducer';
 
 const rootReducer = combineReducers({
   routing,
-  artists
+  artists,
+  tracks
 });
 
 export default rootReducer;
--- app/track/reducer.js
+++ app/track/reducer.js
@@ -1,3 +1,9 @@
-export default function(state = {}, action) {
+import { ADD_TO_NOW_PLAYING } from './actionTypes';
+
+export default function(state = [], action) {
+  switch(action.type) {
+    case ADD_TO_NOW_PLAYING:
+      return [...state, action.selectedTrack];
+  }
   return state;
 }
